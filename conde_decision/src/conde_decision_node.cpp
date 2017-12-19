f17b46e6936305f817d05a1fe967678979ecb690
up201305618 <up201305618@fe.up.pt>
2017-12-19 20:48:57 +0000

    it stops

--- conde_decision/src/conde_decision_node.cpp
+++ conde_decision/src/conde_decision_node.cpp
@@ -1,4 +1,5 @@
 #include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
 #include "std_msgs/String.h"
 #include "std_msgs/Bool.h"
 
@@ -7,16 +8,41 @@
 #include "std_msgs/Float64MultiArray.h"
 
 ros::Publisher dist_angle_pub;
+ros::Publisher publisher;
+bool stop;
 
 void crossWalkCallback(const std_msgs::Bool::ConstPtr& msg)
 {
   ROS_INFO("I heard: [%d]", msg->data);
 }
 
+//////////////////////////////////////////////////////////////////////////////////////// 
+
 void semaphoreCallback(const std_msgs::String::ConstPtr& msg)
 {
   ROS_INFO("I heard: [%s]", msg->data.c_str());
+
+  if (msg->data == "stop")
+    stop = true;
+  else
+    stop = false;
 }
+
+void trackingCallback(const geometry_msgs::Twist msg)
+{
+  geometry_msgs::Twist new_msg;
+  
+  if (stop) {
+    new_msg.linear.x = 0;
+    new_msg.angular.z = 0;
+  } else {
+    new_msg.linear.x = msg.linear.x;
+    new_msg.angular.z = msg.angular.z;
+  }
+  publisher.publish(new_msg);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////// 
 
 void velocityCallback(const std_msgs::Float64MultiArray::ConstPtr& array)    {
     std::cout << array->data[0] << std::endl;       // Distance
@@ -47,7 +73,15 @@ int main(int argc, char **argv)
 
   ros::Subscriber sub1 = n.subscribe("/crossWalk", 5, crossWalkCallback);
   ros::Subscriber sub2 = n.subscribe("/conde_dist_angle", 100, velocityCallback);
+
+//////////////////////////////////////////////////////////////////////////////////////// 
+  
   ros::Subscriber sub3 = n.subscribe("/conde_semaphore_info", 100, semaphoreCallback);
+  ros::Subscriber sub4 = n.subscribe("/conde_tracking_info", 100, trackingCallback);
+
+  publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 2000);
+
+////////////////////////////////////////////////////////////////////////////////////////
 
   dist_angle_pub = n.advertise<std_msgs::Float64MultiArray>("/conde_msg", 1);
 
